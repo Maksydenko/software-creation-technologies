55311ac215c39bd5b4361d7fef17fae2843d3788
Maksym Davydenko <maksydenko@gmail.com>
2023-02-01 04:27:13 +0200

    modified:   lab-01/main.py

--- lab-01/main.py
+++ lab-01/main.py
@@ -7,6 +7,7 @@ class Person:
         except FileNotFoundError as fnfe:
             print(fnfe)
 
+    # Check whether the value can be the float
     @staticmethod
     def is_float(value):
         try:
@@ -15,6 +16,7 @@ class Person:
         except ValueError:
             return False
 
+    # Check whether the value can be the int
     @staticmethod
     def is_int(value):
         try:
@@ -27,15 +29,27 @@ class Person:
     def split_lines(lines):
         lines = [line.rstrip() for line in lines]
         splitted_lines = []
+
         for line in lines:
             splitted_line = line.split()
             splitted_lines.append(splitted_line)
         return splitted_lines
 
+    @staticmethod
+    def count_average(list):
+        len_list = len(list)
+        sum = 0
+
+        for number in list:
+            sum += number
+        average = sum / len_list
+        return average
+
     def read_data(self):
         with open(self.path_file, encoding="utf-8") as file:
             lines = file.readlines()
             lines = [line.rstrip() for line in lines]
+            correct_persons = []
             adults = []
             minors = []
 
@@ -57,11 +71,12 @@ class Person:
                                 adults.append(line + "\n")
                             else:
                                 minors.append(line + "\n")
+                            correct_persons.append(line + "\n")
                     else:
                         continue
                 else:
                     continue
-            return adults, minors
+            return correct_persons, adults, minors
 
     def write_data(self):
         adults = self.read_data()[0]
@@ -74,21 +89,61 @@ class Person:
             with open("minors.txt", "w", encoding="utf-8") as file:
                 file.writelines(minors)
 
+    def count_averages(self):
+        correct_persons = self.read_data()[0]
+        splitted_persons = self.split_lines(correct_persons)
+
+        salaries = []
+        ages = []
+
+        for line in splitted_persons:
+            salary = line[2]
+            salary = int(salary)
+            salaries.append(salary)
+
+            age = line[3]
+            age = int(age)
+            ages.append(age)
+        average_salary = self.count_average(salaries)
+        average_age = self.count_average(ages)
+        average_age = round(average_age)
+        return average_salary, average_age
+
     def __str__(self):
         from tabulate import tabulate
 
         persons = self.persons
-        adults = self.read_data()[0]
-        minors = self.read_data()[1]
+        correct_persons = self.read_data()[0]
+
+        len_persons = len(persons)
+        len_correct_persons = len(correct_persons)
+
+        adults = self.read_data()[1]
+        minors = self.read_data()[2]
+
+        splitted_persons = self.split_lines(correct_persons)
+        splitted_adults = self.split_lines(adults)
+        splitted_minors = self.split_lines(minors)
+
         headers_value = ["Surname", "Name", "Salary", "Age"]
 
-        if len(persons):
+        if len_correct_persons:
+            average_salary = self.count_averages()[0]
+            average_age = self.count_averages()[1]
+
+        if len(correct_persons):
             print(
-                f"All persons:\n{tabulate(self.split_lines(persons), headers=headers_value, tablefmt='grid')}")
-            if len(adults):
-                print(
-                    f"Adults:\n{tabulate(self.split_lines(adults), headers=headers_value, tablefmt='grid')}")
-            if len(minors):
-                print(
-                    f"Minors:\n{tabulate(self.split_lines(minors), headers=headers_value, tablefmt='grid')}")
-        return f"Completed"
+                f"All persons:\n{tabulate(splitted_persons, headers=headers_value, tablefmt='grid')}")
+        if len(adults):
+            print(
+                f"Adults:\n{tabulate(splitted_adults, headers=headers_value, tablefmt='grid')}")
+        if len(minors):
+            print(
+                f"Minors:\n{tabulate(splitted_minors, headers=headers_value, tablefmt='grid')}")
+        if len_correct_persons:
+            print(f"salary: {average_salary}; average age: {average_age}")
+        return f"{len_correct_persons} / {len_persons} correct persons"
+
+
+test = Person("lab-01/persons.txt")
+print(test)
